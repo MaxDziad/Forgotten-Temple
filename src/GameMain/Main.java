f4288fd2a6a39bac2fe2eaa9bea5600a09a85790
MaxDziad <dziadon.kakuzu@gmail.com>
2021-03-28 10:09:52 +0200

    Updating display

--- src/GameMain/Main.java
+++ src/GameMain/Main.java
@@ -1,12 +1,17 @@
 package GameMain;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 public class Main {
 	
 	public static void main(String[] args) {
 		
 		JFrame window = new JFrame("Super Student Bros");
+		//Center game on screen
+		window.setSize(new Dimension(GamePanel.WIDTH, GamePanel.HEIGHT));
+		window.setLocationRelativeTo(null);
+		
 		window.setContentPane(new GamePanel());
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(false);
